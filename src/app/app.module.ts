55538f82d073b494d67bf2cd654a8dfe19032bfb
Luis Hernandez <luis_hernandez@epam.com>
2018-08-14 15:41:55 -0500

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AlertContainerComponent } from './alert-container/alert-container.component';
+import { AlertComponent } from './alert/alert.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AlertContainerComponent,
+    AlertComponent
   ],
   imports: [
     BrowserModule
