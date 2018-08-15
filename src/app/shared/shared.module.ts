7912633e90f86bc521cbe37d267f343be9efae91
Luis Hernandez <luis_hernandez@epam.com>
2018-08-15 11:25:39 -0500

    Register components

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { AlertContainerComponent } from './alert-container/alert-container.component';
+import { AlertComponent } from './alert/alert.component';
+
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: []
+  declarations: [AlertContainerComponent, AlertComponent],
+  exports: [AlertContainerComponent],
+  entryComponents: [AlertComponent]
 })
 export class SharedModule { }
