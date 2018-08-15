83697d5fa41b664f1fae64e1c269ec7fd8091b81
Luis Hernandez <luis_hernandez@epam.com>
2018-08-15 11:35:24 -0500

    Re-add id property

--- src/app/shared/models/alert.model.ts
+++ src/app/shared/models/alert.model.ts
@@ -1,4 +1,5 @@
 export interface Alert {
+  id?: number; // Declared as optional as it will be generated dynamically.
   message: string;
   type: 'success' | 'error' | 'warning' | 'info';
   closable?: boolean;
