90d20782d3b60a8a96edc4d2566bfe6566127197
Luis Hernandez <luis_hernandez@epam.com>
2018-08-15 11:24:44 -0500

    Add superbasic alert service

---
+++ src/app/core/alert.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+
+import { Subject, Observable } from 'rxjs';
+import { Alert } from '../shared/models/alert.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AlertService {
+  alerts$: Observable<Alert>;
+
+  private defaultConfig: Alert;
+  private alertsSubject: Subject<Alert>;
+
+  constructor() {
+    this.alertsSubject = new Subject();
+    this.alerts$ = this.alertsSubject.asObservable();
+    this.defaultConfig = {
+      message: '',
+      type: 'success',
+      closable: true,
+      timeout: 5000,
+    };
+  }
+
+  showAlert(config: Alert): void {
+    const alert = Object.assign({}, this.defaultConfig, config);
+    this.alertsSubject.next(alert);
+  }
+}
