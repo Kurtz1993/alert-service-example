720de02e97355220e3bd0f074aad643c24563fcd
Luis Hernandez <luis_hernandez@epam.com>
2018-08-15 12:03:44 -0500

    Add hostbinding for alert styling

--- src/app/shared/alert/alert.component.ts
+++ src/app/shared/alert/alert.component.ts
@@ -1,4 +1,4 @@
-import { Component, Output, EventEmitter, Input } from '@angular/core';
+import { Component, Output, EventEmitter, Input, HostBinding, OnInit } from '@angular/core';
 import { Alert } from '../models/alert.model';
 
 @Component({
@@ -6,12 +6,17 @@ import { Alert } from '../models/alert.model';
   templateUrl: './alert.component.html',
   styleUrls: ['./alert.component.scss'],
 })
-export class AlertComponent {
+export class AlertComponent implements OnInit {
   @Input()
   alertInformation: Alert;
-
   @Output()
   close = new EventEmitter<number>();
+  @HostBinding('class')
+  componentClasses: string;
+
+  ngOnInit() {
+    this.componentClasses = `alert alert--${this.alertInformation.type}`;
+  }
 
   closeAlert() {
     this.close.emit(this.alertInformation.id);
