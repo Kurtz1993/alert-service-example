a35f581da46ddf9bd63aeaf766c0a25dc5552a9a
Luis Hernandez <luis_hernandez@epam.com>
2018-08-15 11:25:31 -0500

    Add alert-container and a button to app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { AlertService } from './core/alert.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'alert-service';
+
+  constructor(private alertService: AlertService) {}
+
+  createAlert(): void {
+    this.alertService.showAlert({ message: 'hello!!!', type: 'success' });
+  }
 }
