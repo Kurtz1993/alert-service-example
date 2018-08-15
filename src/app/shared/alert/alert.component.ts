5b2a21ac80d221def05c0d158ceb3437436c36db
Luis Hernandez <luis_hernandez@epam.com>
2018-08-15 11:25:20 -0500

    Add basic alert component

---
+++ src/app/shared/alert/alert.component.ts
@@ -0,0 +1,22 @@
+import { Component, Output, EventEmitter, Input } from '@angular/core';
+import { Alert } from '../models/alert.model';
+
+@Component({
+  selector: 'app-alert',
+  templateUrl: './alert.component.html',
+  styleUrls: ['./alert.component.scss'],
+})
+export class AlertComponent {
+  @Input()
+  alertInformation: Alert;
+
+  @Input()
+  id: number;
+
+  @Output()
+  close = new EventEmitter<number>();
+
+  closeAlert() {
+    this.close.emit(this.id);
+  }
+}
